fb7828f202af7dc51fd9c6f538d559e8e1c9569e
Idris Fagbemi <idrisniyi94@gmail.com>
2024-04-28 23:35:25 -0500

    Modified code

--- app.py
+++ app.py
@@ -561,5 +561,5 @@ def site_maintenance():
     response.set_cookie('site-cookie', SESSION_COOKIE_TOKEN)
     return response
 
-if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000)
+# if __name__ == "__main__":
+#     app.run(debug=True, host='0.0.0.0', port=5000)
