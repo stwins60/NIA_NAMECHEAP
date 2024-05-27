971fd1cb3da4a8d1d6d1d155098dd546425e6fe8
Idris Fagbemi <idrisniyi94@gmail.com>
2024-05-27 15:25:11 -0500

    Added sentry

--- app.py
+++ app.py
@@ -33,13 +33,13 @@ sentry_sdk.init(
     dsn="https://5450658eef11bb7d1055b54edfbbf1c7@sentry.africantech.dev/2",
     enable_tracing=True,
     traces_sample_rate=1.0,
+    profiling_sample_rate=1.0,
     integrations = [
         FlaskIntegration(
             transaction_style="url"
         )
     ]
 )
-
 
 def get_random_verse():
     verse = random.randint(1, 6236)
