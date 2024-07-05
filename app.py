41a7286c282940e443f2e8c0f2d2ea02be91128c
Idris Fagbemi <idrisniyi94@gmail.com>
2024-07-04 20:11:07 -0500

    fix database issue

--- app.py
+++ app.py
@@ -25,7 +25,13 @@ CORS(app)
 token = ''.join(random.sample('abcdefghijklmnopqrstuvwxyz1234567890', 32))
 app.secret_key = token
 
+DB_HOST = os.getenv('DB_HOST')
+DB_PORT = os.getenv('DB_PORT')
+DB_USER = os.getenv('DB_USER')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
+DB_NAME = os.getenv('DB_NAME')
 
+db_pass = DB_PASSWORD.replace('@', '%40')
 
 
 headers = {
