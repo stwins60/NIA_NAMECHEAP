6488d874a4877edabc752c5f9a6d3752e8b57741
Idris Fagbemi <idrisniyi94@gmail.com>
2024-09-30 00:32:46 -0500

    modified

--- server.py
+++ server.py
@@ -24,7 +24,7 @@ if __name__ == '__main__':
                         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
       serve(app.app, host='0.0.0.0', port=5000, threads=6, 
-            url_scheme='http', backlog=2048, max_request_header_size=4096, 
+            url_scheme='https', backlog=2048, max_request_header_size=4096, 
             max_request_body_size=1073741824, connection_limit=1000, 
             cleanup_interval=30, channel_timeout=120, asyncore_loop_timeout=1, 
             asyncore_use_poll=True, expose_tracebacks=False, log_socket_errors=True, 
